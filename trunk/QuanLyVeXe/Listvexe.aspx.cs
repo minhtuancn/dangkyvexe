513d4a56c9c2895ba56bf492ecb0689b047a6422
bahieu.it.dlu@gmail.com <bahieu.it.dlu@gmail.com@6f03b424-d02d-323e-979c-a00e65e0cc75>
2012-12-03 02:52:18 +0000

    Danh sachs vé xe tài khoản đó đã đăg ký

--- trunk/QuanLyVeXe/Listvexe.aspx.cs
+++ trunk/QuanLyVeXe/Listvexe.aspx.cs
@@ -4,11 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using DAL;
 
 public partial class Listvexe : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            //   LoadControl();
+        }
+        if (User.Identity.Name != "")
+        {
+            MembershipUser m = Membership.GetUser(User.Identity.Name);
+            lbAcountDangNhap.Text = "" + m.UserName.ToString();
+            LoadControl();
+        }
+    }
+    private void LoadControl()
+    {
+        RepeaterListVeXe.DataSource = VeXe.VeXe_SlectAcountKH(lbAcountDangNhap.Text);
+        RepeaterListVeXe.DataBind();
+    }
+    protected void btDelte_Click(object sender, EventArgs e)
+    {
+        string stringId = Request.Form["Id"] ?? "";
+        if (stringId != "")
+        {
+            foreach (string item in stringId.Split(','))
+            {
+                VeXe.Delete(item);
+                LoadControl();
+            }
+        }
     }
 }
