362b3198c8856b3c1f8db572154e6527507be013
hahd.ctk33 <hahd.ctk33@6f03b424-d02d-323e-979c-a00e65e0cc75>
2012-12-03 10:32:50 +0000

    Xuất thông tin bảng giá vé

--- trunk/QuanLyVeXe/Banggiave.aspx.cs
+++ trunk/QuanLyVeXe/Banggiave.aspx.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DAL;
 
 public partial class Banggiave : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            LoadControl();
+        }
+    }
+    private void LoadControl()
+    {
+        RepeatervBangGiaVe.DataSource = TuyenDi.ListTuyenDi();
+        RepeatervBangGiaVe.DataBind();
     }
 }
